fe8b8ef5c28c69afa395f2b72f3087aa03246187
xuezhenchun <12345678>
2019-07-27 16:27:36 +0800

    添加param注解

--- src/main/java/com/weixin/reward/dao/UserInfoDao.java
+++ src/main/java/com/weixin/reward/dao/UserInfoDao.java
@@ -2,6 +2,7 @@ package com.weixin.reward.dao;
 
 import com.weixin.reward.bean.UserInfo;
 import com.weixin.reward.bean.UserMessage;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -11,5 +12,5 @@ public interface UserInfoDao {
    public void saveUserInfo(UserInfo userInfo);
    public UserInfo getUserInfoByOpenid(String openid);
    public void addUserMessage(UserMessage userMessage);
-   public List<UserMessage> getUserMessageListByOpenid(String openid);
+   public List<UserMessage> getUserMessageListByOpenid(@Param("openid") String openid);
 }
